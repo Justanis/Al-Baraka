0caca6771ad4c3bd8b14d73ec515dd8ea469ff5f
Justanis <anismokhtari32@gmail.com>
2025-05-05 02:21:04 +0200

    Initial commit

--- backend/app/mailer.py
+++ backend/app/mailer.py
@@ -11,7 +11,7 @@ class SMTPMailer:
         app.config['MAIL_PORT'] = 587
         app.config['MAIL_USE_TLS'] = True
         app.config['MAIL_USE_SSL'] = False
-        app.config['MAIL_USERNAME'] = 'al.baraka.teamwork@gmail.com'
+        app.config['MAIL_USERNAME'] = 'al.baraka.teamwork@gmail.com' # Your Gmail address
         app.config['MAIL_PASSWORD'] = 'mpzugpdcadcbkuoa'  # Use App Password, NOT Gmail password
         app.config['MAIL_DEFAULT_SENDER'] = 'al.baraka.teamwork@gmail.com'  # FIXED!
 
